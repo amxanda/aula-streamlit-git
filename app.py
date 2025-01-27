f300693ab2b9aa40c5f064febc1be038df641d3d
Amanda <107783538+amxanda@users.noreply.github.com>
2025-01-27 20:00:13 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.header("Calculadora", divider="gray")
+
+expression = st.text_input("Entre com a expressão")
+
+if (expression):
+  result = eval(expression)
+
+  st.write("Resultado: ", result)
